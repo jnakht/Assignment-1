2c489f37dd871ab7b65f9070a15b141fbc97ca55
jnakht <jisan.cse.cu@gmail.com>
2025-05-08 16:37:15 +0600

    till problem 6, but without one problem

--- Solutions.ts
+++ Solutions.ts
@@ -17,14 +17,95 @@ function filterByRating(items: {title: string; rating: number}[]): {title: strin
 }
 
 
-const books = [
-    {title: 'book1', rating: 4.5},
-    {title: 'book2', rating: 5},
-    {title: 'book3', rating: 3.9},
-    {title: 'book4', rating: 4},
-    {title: 'book4', rating: 2.5},
+// problem 3 start
 
-]
-console.log(filterByRating(books));
+
+
+
+
+
+
+// problem 3 end
+
+
+
+//problem 4 start
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+    getInfo() {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+}
+class Car extends Vehicle {
+    private model: string;
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+    }
+    getModel() {
+        console.log(`Model: ${this.model}`);
+    }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+// myCar.getModel();  // Output: "Model: Corolla"
+
+// problem 4 end
+
+
+
+// problem 5 start
+
+function processValue(value: string | number): number {
+    if (typeof value === 'string') {
+        return value.length;
+    }  else if (typeof value === 'number') {
+        return value * 2;
+    } else {
+        throw new Error("Invalid Type");
+    }  
+}
+
+// problem 5 end
+
+
+// problem 6 start
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+        return null;
+    }
+    let mostExpensiveProduct: Product = products[0];
+    products.map((product: Product, index: number) => {
+        // if multiple products appear with same price, the last one of them will be selected
+        if (product.price >= mostExpensiveProduct.price) {
+            mostExpensiveProduct = product;
+        }
+    })
+    return mostExpensiveProduct;
+}
+
+
+// problem 6 end 
+
+
+
+// problem 7 start 
+
+
+
+
+
+// problem 7 end
 
 
